a166dea236b215b81f4b8e37ee9d53d525864a79
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-20 04:39:00 +0300

    nixos/configuration: add back xdg-desktop-portal-hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -129,7 +129,10 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+    ];
   };
 
   # Open ports in the firewall.
